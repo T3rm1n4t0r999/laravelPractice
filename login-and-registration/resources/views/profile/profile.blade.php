5c216f24f7076e24627d7e3918462874472a90c8
M1000 <bel0snejka04@vk.com>
2025-03-07 16:02:24 +0300

    1

--- login-and-registration/resources/views/profile/profile.blade.php
+++ login-and-registration/resources/views/profile/profile.blade.php
@@ -1,11 +1,7 @@
 @extends('layouts.app-master')
 @section('content')
     <div class="bg-light p-5 rounded">
-        <form method='post' action="/file.php" enctype="multipart/form-data">
-        <input type="hidden" name="MAX_FILE_SIZE" value="5000000">
-        <input type='file' name='file[]' class='file-drop' id='file-drop' multiple required><br>
-        <input class="btn btn-lg btn-primary" type='submit' value='Загрузить' >
-        </form>
+
     </div>
 @endsection
 
